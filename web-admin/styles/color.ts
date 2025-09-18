2effc701cf5818326c32d9baff06f814e864059a
HuongNguyen0828 <huongnguyen.cada@gmail.com>
2025-09-18 17:11:24 -0600

    web-admin: adding SignUp page and color style

---
+++ web-admin/styles/color.ts
@@ -0,0 +1,19 @@
+// lib/theme.ts
+const theme = {
+  colors: {
+    primary: "#1e90ff", // sky blue
+    secondary: "#4caf50", // green
+    warning: "#ff9800", // orange
+    error: "#d32f2f",
+    background: "#fff",
+    text: "#333",
+    backgroundAlt: "linear-gradient(to bottom right, #f9f9f9, #e0f7fa)", // Light gradient background
+  },
+  fonts: {
+    heading: "'Poppins', sans-serif",
+    body: "'Inter', sans-serif",
+  },
+  spacing: (factor: number) => `${0.25 * factor}rem`, // 1 = 4px
+};
+
+export default theme;
