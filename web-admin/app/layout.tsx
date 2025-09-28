80e5d452df2769fd33b468be40595b5175c81c81
HuongNguyen0828 <huongnguyen.cada@gmail.com>
2025-09-28 16:55:34 -0600

    web-admin: fix previous error of props frozen
    to wrap layout inside of <Provider> including Google Maps services and AuthContext

--- web-admin/app/layout.tsx
+++ web-admin/app/layout.tsx
@@ -1,27 +1,11 @@
-// app/layout.tsx
-'use client';
 import type { ReactNode } from "react";
-import { AuthProvider } from "@/lib/auth"; 
-import { LoadScript } from "@react-google-maps/api"; // for using AutoComplete api throughout the web-app
+import Providers from "./provider"; // client wrapper for Auth and Google Maps API
 
-
-// Wrap the entire app with AuthProvider
 export default function RootLayout({ children }: { children: ReactNode }) {
-  // Protecting the entire app with AuthProvider and Admin check4
-  
   return (
     <html lang="en">
       <body>
-        <AuthProvider>
-          {/* Wrap web-admin app with AuthContext, and then LoadScript */}
-          {/* Only load once, otherwise, error */}
-          <LoadScript 
-          googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
-          libraries={["places"]}
-          >
-            {children}
-          </LoadScript>
-        </AuthProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
