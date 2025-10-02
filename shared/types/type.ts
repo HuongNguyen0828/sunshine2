f64825faae1c691e3692bf140ace2279cf5916e6
HuongNguyen0828 <huongnguyen.cada@gmail.com>
2025-10-02 17:55:25 -0600

    shared: re-organize data types

--- shared/types/type.ts
+++ shared/types/type.ts
@@ -1,32 +1,13 @@
-
-
-type EntryType = "Attendance" | "Schedule_note" |  "Food" | "Photo" | "Sleep" | "Toilet" | "Supply Request";
+type EntryType =
+  | "Attendance"
+  | "Schedule_note"
+  | "Food"
+  | "Photo"
+  | "Sleep"
+  | "Toilet"
+  | "Supply Request";
 type AttendanceSubtype = "Check in" | "Check out";
-type FoodSubtype = "Breakfast" | "Lunch" | "Snack"
-
-
-
-export type Admin = {
-  daycareId: string, // referencing Daycare Provider 
-  locationId: string[] // referencing location id, ['*'] for all location
-  firstName: string,
-  lastName: string, 
-  email: string,
-  phone: string,
-  address: string,
-  postalCode: string,
-}
-
-export type Entry = {
-  id: string;
-  childId: string;
-  staffId: string;
-  type: EntryType;
-  subtype?: AttendanceSubtype | FoodSubtype;
-  detail?: string;
-  photoUrl?: string;
-  createdAt: string;
-};
+type FoodSubtype = "Breakfast" | "Lunch" | "Snack";
 
 export type DaycareProvider = {
   id: string;
@@ -36,43 +17,45 @@ export type DaycareProvider = {
   phone: string;
   email: string;
   contactName: string;
-}
+};
+// Remove Location for now for purpose of simplicity
+export type Location = {
+  id: string;
+  name: string;
+  phone: string;
+  email: string;
+  providerId: string;
+  street: string;
+  city: string;
+  provine: string;
+  zip: string;
+  country: string;
+  creditCardInfo?: string;
+};
 
-
- /////// Remove Location for now for purpose of simplicity
-// export type Location = { 
-//   id: string;
-//   name: string;
-//   phone: string;
-//   email: string;
-//   providerId: string;
-//   street: string;
-//   city: string;
-//   provine: string;
-//   zip: string;
-//   country: string;
-//   creditCardInfo?: string; 
-// }
+export type Admin = {
+  daycareId: string; // referencing Daycare Provider , ['*'] means all daycare, only for Sunshine admin
+  locationId: string[]; // referencing location id, ['*'] for all location
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  address: string;
+  postalCode: string;
+};
+// Upper, For Sunshine admin only read/ write access ====================================================================
+// The rest if for admin of daycare/ daycare location having read and write access. Sunshine admin just have read access
 
 export type Class = {
   id: string;
   name: string;
-  locationId: string; 
+  locationId: string;
   capacity: number;
   volume: number;
   ageStart: number;
   ageEnd: number;
-  classroom: string; 
-}
-
-export type Schedule = {
-  id: string;
-  classId: string;
-  dayOfWeek: number; // 0 (Sunday) to 6 (Saturday)
-  startTime: string; // "HH:MM" format
-  endTime: string;   // "HH:MM" format
-  morningAfernoon: "Morning" | "Afternoon" | "Full day";
-}
+  classroom: string;
+};
 
 export type Teacher = {
   id: string;
@@ -90,18 +73,16 @@ export type Teacher = {
   locationId?: string; // Location assigned to this staff
   startDate: string;
   endDate?: string; // Optional end date for staff
+};
 
-}
-export type monthlyReport = {
+export type Schedule = {
   id: string;
-  childId?: string; // Optional, if not provided, means all children under the parent
-  classId?: string; // Optional, if not provided, means all classes under the location
-  locationId?: string; // Optional, if not provided, means all locations under the provider
-  month: string; // "YYYY-MM" format
-  attendanceCount: number;
-  creatAt: string;
-  updatedAt?: string;
-}
+  classId: string;
+  dayOfWeek: number; // 0 (Sunday) to 6 (Saturday)
+  startTime: string; // "HH:MM" format
+  endTime: string; // "HH:MM" format
+  morningAfernoon: "Morning" | "Afternoon" | "Full day";
+};
 
 export type Child = {
   id: string;
@@ -116,7 +97,8 @@ export type Child = {
   enrollmentDate: string; // ISO date string
   enrollmentStatus: "Active" | "Withdraw" | "New" | "Waitlist";
   endDate?: string; // ISO date string
-}
+};
+
 export type Parent = {
   id: string;
   firstName: string;
@@ -132,20 +114,41 @@ export type Parent = {
   country: string;
   emergencyContact?: string;
   createdAt: string; // ISO date string
-  updatedAt?: string; // ISO date string  
+  updatedAt?: string; // ISO date string
   preferredLanguage?: string; // e.g., "en", "fr"
-}
-export type DailyReport ={
+};
+
+export type Entry = {
+  id: string;
+  childId: string;
+  staffId: string;
+  type: EntryType;
+  subtype?: AttendanceSubtype | FoodSubtype;
+  detail?: string;
+  photoUrl?: string;
+  createdAt: string;
+};
+export type Photo = {
+  id: string;
+  entryId: string;
+  url: string;
+  uploadedAt: string; // ISO date string
+};
+export type DailyReport = {
   id: string;
   childId: string;
   date: string; // ISO date string
   entries: Entry[];
   createdAt: string; // ISO date string // report created right after kid is checked out
-}
-export type Photo = {
+};
+
+export type monthlyReport = {
   id: string;
-  entryId: string;
-  url: string;
-  uploadedAt: string; // ISO date string  
-  
-}
+  childId?: string; // Optional, if not provided, means all children under the parent
+  classId?: string; // Optional, if not provided, means all classes under the location
+  locationId?: string; // Optional, if not provided, means all locations under the provider
+  month: string; // "YYYY-MM" format
+  attendanceCount: number;
+  creatAt: string;
+  updatedAt?: string;
+};
