0abdf10bc355b35f66889fadf241e8d44e1a9cee
HuongNguyen0828 <huongnguyen.cada@gmail.com>
2025-10-08 15:44:03 -0600

    web-admin: allowing to remember idToken from Cookies
    handle Case when users away and comeback, still remember me by using Cookies

--- web-admin/api/client.ts
+++ web-admin/api/client.ts
@@ -2,9 +2,10 @@
 "use client";
 
 import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
+import Cookies from "js-cookie";
 
 const API_BASE = process.env.NEXT_PUBLIC_API_BASE ?? "";
-// Wait for Firebase Auth to initialize and get current user
+// First: Wait for Firebase Auth to initialize and get current user
 async function waitUser(): Promise<User | null> {
   const auth = getAuth();
   if (auth.currentUser) return auth.currentUser;
@@ -23,6 +24,20 @@ async function waitUser(): Promise<User | null> {
   });
 }
 
+// Then, fallbacke to Cookies to get idToken if user already login in and page reload/ open in new tab/window/close and re-open tab/etc.
+async function getIdToken(): Promise<string | null> {
+  // Case login and user is currentUser
+  const user = await waitUser();
+  if (user) return user.getIdToken(); // This is idToken from Firebase Auth SDK
+
+  // Case Fallback to Cookies
+  const idToken = Cookies.get("idToken");
+  console.log("Fallback to Cookies idToken:", idToken);
+  return idToken ?? null; // This is idToken stored in Cookies
+}
+
+
+
 async function request<T>(path: string, init?: RequestInit, authRequired = true): Promise<T> {
   const url = `${API_BASE}${path.startsWith("/") ? path : `/${path}`}`;
   const headers: Record<string, string> = {
@@ -32,8 +47,11 @@ async function request<T>(path: string, init?: RequestInit, authRequired = true)
   };
 
   if (authRequired) {
-    const u = await waitUser();
-    const token = u ? await u.getIdToken() : null;
+    // const u = await waitUser();
+    // const token = u ? await u.getIdToken() : null;
+
+    // Token is wrapped: Either from Firebase Auth SDK or from Cookies
+    const token = await getIdToken();
     if (!token) throw new Error("Not authenticated");
     headers.Authorization = `Bearer ${token}`;
   }
