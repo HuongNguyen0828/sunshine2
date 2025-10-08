3f17c261a54089aedb7e8a47003d874afe307805
HuongNguyen0828 <huongnguyen.cada@gmail.com>
2025-10-08 13:39:43 -0600

    backend: update Midlware and web-admin: update app/page
     midleware: req.user instead of req.auth to match auth-controller in check-email, get-moble, get-admin: if (!req.user) check-email

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -24,33 +24,41 @@ export const authMiddleware = async (
   res: Response,
   next: NextFunction
 ) => {
+  const authHeader = req.headers.authorization;
+
+  // If no input header
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .send({ message: "Missing or invalid Authorization header" });
+  }
+
+  // Else, if have header starting with "Bearer ", extract the token at index [1],
+  const idToken = authHeader.split("Bearer ")[1];
+  if (!idToken)
+    return res
+      .status(401)
+      .send({ message: "Missing or invalid idToken in Authorization header" });
+
+  // Else if idToken is string
   try {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res
-        .status(401)
-        .json({ message: "Missing or invalid Authorization header" });
-    }
-
-    const idToken = authHeader.slice("Bearer ".length).trim();
-    if (!idToken) {
-      return res
-        .status(401)
-        .json({ message: "Missing ID token in Authorization header" });
-    }
-
     const decoded = await admin.auth().verifyIdToken(idToken);
     const email = decoded.email ?? null;
-
     const role = await findRoleByEmail(email);
-    if (!role) {
-      return res.status(403).json({ message: "Unauthorized email" });
+
+    if (!role) return res.status(403).send({ message: "Unauthorized email!" });
+
+    // Adding: check if admin accessing their own resource
+      // Case: if not passing uid in rquest link, and 
+      // Case: if uid passing in  request link, but doesn't match the uid of user login (from decode JWT)
+    if (req.params.uid && req.params.uid !== decoded.uid) {
+      return res.status(403).send({ message: "Forbidden: cannot access other user's data" });
     }
 
-    req.auth = { uid: decoded.uid, email, role };
-    return next();
+    req.user = { uid: decoded.uid, email: email!, role };
+    // Room for call next function as getRole from controller
+    next();
   } catch (err) {
-    console.error("[authMiddleware] verifyIdToken error:", err);
-    return res.status(401).json({ message: "Invalid token" });
+    return res.status(401).send({ message: "Invalid token" });
   }
 };
