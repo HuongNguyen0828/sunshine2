ca5def6e57f71fc91014d34d1d94a972572f07af
Yue Zhou@Bitravage <amirzhou001@gmail.com>
2025-11-02 15:55:31 -0700

    checkpoint: modify mobile/app/(teacher)/(tabs)/messages.tsx - 15:55

--- mobile/app/(teacher)/(tabs)/messages.tsx
+++ mobile/app/(teacher)/(tabs)/messages.tsx
@@ -509,6 +509,7 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(255, 255, 255, 0.9)",
     borderRadius: 16,
     padding: 16,
+    marginHorizontal: 20,
     marginBottom: 12,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
