dbd256b56cab9928335c78f8f1b1a26226215ea3
Yue Zhou@Paraflux <yue@paraflux.ca>
2025-11-25 12:38:18 -0700

    checkpoint: modify web-admin/components/scheduler/WeeklyCalendar.tsx - 12:38

--- web-admin/components/scheduler/WeeklyCalendar.tsx
+++ web-admin/components/scheduler/WeeklyCalendar.tsx
@@ -308,8 +308,8 @@ export function WeeklyCalendar({
                             }}
                             onDragOver={(e) => handleDragOver(e, schedule)}
                             onDrop={(e) => handleDrop(e, schedule)}
-                            className={`group relative rounded-lg px-3 py-2 ${
-                              openMenuId === schedule.id || draggedSchedule?.id === schedule.id ? '' : 'shadow-sm'
+                            className={`group relative px-3 py-2 ${
+                              openMenuId === schedule.id || draggedSchedule?.id === schedule.id ? '' : ''
                             }`}
                             style={{
                               backgroundColor: schedule.activity?.color + '20',
