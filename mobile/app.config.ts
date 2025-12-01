208bd44fbf3ae5976941b399e36a3356e5e983e0
DongjeenHan <dongjeenhan@gmail.com>
2025-12-01 13:23:49 -0700

    Update app.config.ts

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -4,8 +4,37 @@ const config: ExpoConfig = {
   name: "Sunshine",
   slug: "sunshine",
   scheme: "sunshine",
-  plugins: ["expo-router"],
-  experiments: { typedRoutes: true },
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
+  },
+  ios: {
+    supportsTablet: true
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff"
+    },
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false
+  },
+  web: {
+    favicon: "./assets/favicon.png"
+  },
+  plugins: [
+    "expo-router",
+    "expo-notifications"
+  ],
+  experiments: {
+    typedRoutes: true
+  },
   extra: {
     firebase: {
       apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
